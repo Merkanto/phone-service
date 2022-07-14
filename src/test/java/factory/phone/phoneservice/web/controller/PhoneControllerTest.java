ab371ee629b95937c4d0033ce1b4fbb890829bb0
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-14 15:00:10 +0300

    Fixing Tests. Closes #11.

--- src/test/java/factory/phone/phoneservice/web/controller/PhoneControllerTest.java
+++ src/test/java/factory/phone/phoneservice/web/controller/PhoneControllerTest.java
@@ -2,12 +2,14 @@ package factory.phone.phoneservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import factory.phone.phoneservice.web.model.PhoneDto;
+import factory.phone.phoneservice.web.model.PhoneStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -33,7 +35,7 @@ class PhoneControllerTest {
     @Test
     void saveNewPhone() throws Exception {
 
-        PhoneDto phoneDto = PhoneDto.builder().build();
+        PhoneDto phoneDto = getValidPhoneDto();
         String phoneDtoJson = objectMapper.writeValueAsString(phoneDto);
 
         mockMvc.perform(post("/api/v1/phone/")
@@ -44,12 +46,21 @@ class PhoneControllerTest {
 
     @Test
     void updatePhoneById() throws Exception {
-        PhoneDto phoneDto = PhoneDto.builder().build();
+        PhoneDto phoneDto = getValidPhoneDto();
         String phoneDtoJson = objectMapper.writeValueAsString(phoneDto);
 
         mockMvc.perform(put("/api/v1/phone/" + UUID.randomUUID().toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(phoneDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    PhoneDto getValidPhoneDto() {
+        return PhoneDto.builder()
+                .phoneName("My Phone")
+                .phoneStyle(PhoneStyleEnum.Monoblock)
+                .price(new BigDecimal("9.99"))
+                .imei(991753880596726L)
+                .build();
     }
 }

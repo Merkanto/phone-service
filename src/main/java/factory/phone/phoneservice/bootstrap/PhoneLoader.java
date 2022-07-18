f38dc6ad5cba2a19525085e4bdfbcac0034a15a9
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-18 09:19:46 +0300

    Adding init data script. Closes #17.

--- src/main/java/factory/phone/phoneservice/bootstrap/PhoneLoader.java
+++ src/main/java/factory/phone/phoneservice/bootstrap/PhoneLoader.java
@@ -6,13 +6,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-@Component
+//@Component
 public class PhoneLoader implements CommandLineRunner {
 
     public static final String PHONE_1_IMEI = "338694371652036";
     public static final String PHONE_2_IMEI = "334015010517204";
     public static final String PHONE_3_IMEI = "863571738204276";
+    public static final UUID PHONE_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID PHONE_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID PHONE_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final PhoneRepository phoneRepository;
 
